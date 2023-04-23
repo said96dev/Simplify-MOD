506ee5e03b8a3d9f6f649116030c6a0615ddb3f0
said96dev <alhendisaid@gmail.com>
2023-04-23 18:23:20 +0200

    23.04.2023

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -1,19 +1,21 @@
-import express from "express";
+import express from 'express'
 import {
-    createTask,
-    deleteTask , 
-    getSingleTask,
-    updateTask,
-    getAllTasks
-}  from "../controllers/taskController.js"
+  createTask,
+  deleteTask,
+  getSingleTask,
+  updateTask,
+  getAllTasks,
+} from '../controllers/taskController.js'
 
-import {authorizePermissions } from "../middleware/authentication.js";
+import { authorizePermissions } from '../middleware/authentication.js'
 
 const router = express.Router()
 
-router.route("/").get(getAllTasks) //users can see there Task, admin can see all Tasks
-router.route("/addTask").post( authorizePermissions("admin") ,createTask)
-router.route("/:id").get(getSingleTask).delete(deleteTask).patch(updateTask)
+router.route('/').get(getAllTasks) //users can see there Task, admin can see all Tasks
+//router.route("/addTask").post( authorizePermissions("admin") ,createTask)
+router.route('/addTask').post(createTask) // to render
+
+router.route('/:id').get(getSingleTask).delete(deleteTask).patch(updateTask)
 
 //weiterleiten
 
